f8b29bb3d485ce50107997afccd6061822a5e148
Lee <likun7981@gmail.com>
2017-05-06 20:40:06 +0800

    123123123

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,42 +2,44 @@ import 'whatwg-fetch';
 import { stringify } from 'qs';
 // 本工具类只能用于接收和处理json数据，其他数据请使用fetch原生;
 
-const noop = () => { };
+const noop = () => {};
 
 let globalCallbacks = {
   onStart: noop,
   onComplete: noop,
   onSuccess: noop,
   onError: noop,
-  onSuccessFilter: result => result
+  onSuccessFilter: result => result,
 };
 let globalConifg = {};
-export const config = ({
-  method = 'GET',
-  headers,
-  credentials = 'omit',
-  body,
-  onStart = noop,
-  onComplete = noop,
-  onError = noop,
-  onSuccessFilter = result => result,
-  onSuccess = noop
-} = {}) => {
+export const config = (
+  {
+    method = 'GET',
+    headers,
+    credentials = 'omit',
+    body,
+    onStart = noop,
+    onComplete = noop,
+    onError = noop,
+    onSuccessFilter = result => result,
+    onSuccess = noop,
+  } = {}
+) => {
   globalCallbacks = {
     onStart,
     onComplete,
     onSuccess,
     onError,
-    onSuccessFilter
+    onSuccessFilter,
   };
   globalConifg = {
     method,
     headers,
     body,
-    credentials
+    credentials,
   };
 };
-const catchCallbackFn = fn => (params) => {
+const catchCallbackFn = fn => params => {
   try {
     fn(params);
   } catch (error) {
@@ -46,57 +48,61 @@ const catchCallbackFn = fn => (params) => {
     throw error;
   }
 };
-const request = (
-  {
-    headers = {},
-    credentials = 'omit'
-  } = {}) => (urlWithMethod, params = {}) => {
-    let [method, url] = urlWithMethod.split(' '); // eslint-disable-line
-    const { onStart, onComplete, onSuccess, onError, onSuccessFilter } = globalCallbacks;
-    const assignHeaders = globalConifg.headers ? {
+const request = ({ headers = {}, credentials = 'omit' } = {}) => (urlWithMethod, params = {}) => {
+  let [method, url] = urlWithMethod.split(' '); // eslint-disable-line
+  const { onStart, onComplete, onSuccess, onError, onSuccessFilter } = globalCallbacks;
+  const assignHeaders = globalConifg.headers
+    ? {
       ...globalConifg.headers,
-      ...headers
-    } : headers;
-    const assignBody = globalConifg.body ? {
-      ...globalConifg.body,
-      ...params
-    } : params;
-    const options = {
-      ...globalConifg,
-      headers: assignHeaders,
-      method,
-      credentials
-    };
-    assignBody = JSON.stringify(JSON.parse(JSON.stringify(assignBody)));
-    if (Object.keys(assignBody).length) {
-      if (method.toUpperCase() !== 'GET') {
-        options.body = assignBody;
-      } else {
-        url += `?${stringify(assignBody)}`;
-      }
+      ...headers,
     }
-    const promise = new Promise((resolve, reject) => {
-      onStart();
-      fetch(url, options).then((response) => {
+    : headers;
+  let assignBody = globalConifg.body
+    ? {
+      ...globalConifg.body,
+      ...params,
+    }
+    : params;
+  const options = {
+    ...globalConifg,
+    headers: assignHeaders,
+    method,
+    credentials,
+  };
+  assignBody = JSON.stringify(JSON.parse(JSON.stringify(assignBody)));
+  if (Object.keys(assignBody).length) {
+    if (method.toUpperCase() !== 'GET') {
+      options.body = assignBody;
+    } else {
+      url += `?${stringify(assignBody)}`;
+    }
+  }
+  const promise = new Promise((resolve, reject) => {
+    onStart();
+    fetch(url, options).then(
+      response => {
         if (response.ok) {
-          response.json().then((result) => {
-            const filterResult = onSuccessFilter(result);
-            if (Object.prototype.toString.call(filterResult) === '[object Error]') {
-              onComplete(filterResult);
-              onError(filterResult);
-              reject(filterResult);
-            } else {
-              onSuccess(filterResult);
-              onComplete(null, filterResult);
-              resolve(filterResult);
+          response.json().then(
+            result => {
+              const filterResult = onSuccessFilter(result);
+              if (Object.prototype.toString.call(filterResult) === '[object Error]') {
+                onComplete(filterResult);
+                onError(filterResult);
+                reject(filterResult);
+              } else {
+                onSuccess(filterResult);
+                onComplete(null, filterResult);
+                resolve(filterResult);
+              }
+            },
+            error => {
+              const e = new Error('服务器返回数据错误');
+              onComplete(e);
+              onError(e);
+              reject(e);
+              throw new Error(error);
             }
-          }, (error) => {
-            const e = new Error('服务器返回数据错误');
-            onComplete(e);
-            onError(e);
-            reject(e);
-            throw new Error(error);
-          });
+          );
         } else {
           console.log(response);
           const error = new Error(response.statusText);
@@ -105,33 +111,38 @@ const request = (
           onComplete(error);
           reject(error);
         }
-      }, (error) => {
+      },
+      error => {
         onError(error);
         onComplete(error);
         reject(error);
-      });
+      }
+    );
+  });
+  promise.success = fn => {
+    promise.then(result => {
+      if (typeof fn === 'function') {catchCallbackFn(fn)(result);}
     });
-    promise.success = (fn) => {
-      promise.then((result) => {
-        if (typeof fn === 'function') catchCallbackFn(fn)(result);
-      });
-      return promise;
-    };
-    promise.error = (fn) => {
-      promise.then(null, (error) => {
-        if (typeof fn === 'function') catchCallbackFn(fn)(error);
-      });
-      return promise;
-    };
-    promise.complete = (fn) => {
-      fn = (typeof fn === 'function') ? catchCallbackFn(fn) : noop;
-      promise.then((result) => {
-        fn(null, result);
-      }, (error) => {
-        fn(error);
-      });
-      return promise;
-    };
     return promise;
   };
+  promise.error = fn => {
+    promise.then(null, error => {
+      if (typeof fn === 'function') {catchCallbackFn(fn)(error);}
+    });
+    return promise;
+  };
+  promise.complete = fn => {
+    fn = typeof fn === 'function' ? catchCallbackFn(fn) : noop;
+    promise.then(
+      result => {
+        fn(null, result);
+      },
+      error => {
+        fn(error);
+      }
+    );
+    return promise;
+  };
+  return promise;
+};
 export default request;
